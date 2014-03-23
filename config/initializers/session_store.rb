e246d9ffdd23676f77d7f78ae42b25d8d8cf9f9b
Javier C Rodriguez <alu0100505023@ull.edu.es>
2014-03-23 16:30:49 +0000

    Subiendo practica modificada con css

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Prct4Rails::Application.config.session_store :cookie_store, key: '_prct4-rails_session'
---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'prct4-rails'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('prct4-rails')
+    end
+	it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
+    end
+  end
+
+	describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Ayuda')
+    end
+  end	
+
+	describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+  end
+	
+end
