0ce73060cfb8deb23c5ef89ae1380f647aefb432
Javier C Rodriguez <alu0100505023@ull.edu.es>
2014-03-30 21:08:25 +0100

    añadiendo tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,8 +6,9 @@ describe "Static pages" do
 
     it "should have the content 'prct4-rails'" do
       visit '/static_pages/home'
-      expect(page).to have_content('prct4-rails')
+      expect(page).to have_content('home')
     end
+
 	it "should not have a custom page title" do
       visit '/static_pages/home'
       expect(page).not_to have_title('| Home')
@@ -29,5 +30,18 @@ describe "Static pages" do
       expect(page).to have_content('About Us')
     end
   end
+
+	describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+    end
+	end
 	
 end
